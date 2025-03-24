21cbf53f839f9162680c99d5bdf064d055704cdb
Iker <iker.a@opendeusto.es>
2025-03-24 16:36:08 +0100

    Cambios en las plantillas para uso de thymeleaf, asi como estilos de CSS

---
+++ src/main/java/com/example/restapi/controller/WebController.java
@@ -0,0 +1,48 @@
+package com.example.restapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.restapi.model.Pelicula;
+import com.example.restapi.model.Series;
+import com.example.restapi.service.DeustoStreamService;
+
+@Controller
+public class WebController {
+
+    @Autowired
+    private DeustoStreamService deustoStreamService;
+
+    @GetMapping("/peliculas")
+    public String mostrarPeliculas(Model model) {
+        List<Pelicula> peliculas = deustoStreamService.getAllPeliculas();
+        model.addAttribute("peliculas", peliculas);
+        return "peliculas"; // Retorna el nombre del archivo HTML en /resources/templates/
+    }
+
+    @GetMapping("/series")
+    public String mostrarSeries(Model model) {
+        model.addAttribute("series", deustoStreamService.getAllSeries());
+        return "series";
+    }
+
+    @GetMapping("/usuarios")
+    public String mostrarUsuarios(Model model) {
+        model.addAttribute("usuarios", deustoStreamService.getAllUsuarios());
+        return "usuarios";
+    }
+    @GetMapping("/catalogo")
+    public String mostrarCatalogo(Model model) {
+        List<Pelicula> peliculas = deustoStreamService.getAllPeliculas();
+        List<Series> series = deustoStreamService.getAllSeries();
+
+        model.addAttribute("peliculas", peliculas);
+        model.addAttribute("series", series);
+
+        return "catalogo"; // Mapea a catalogo.html en /templates/
+    }
+}
