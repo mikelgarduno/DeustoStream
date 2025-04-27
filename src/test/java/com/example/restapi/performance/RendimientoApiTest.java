c7c0f96a265c698b1f54f38e192784190ac44164
Iker <iker.a@opendeusto.es>
2025-04-27 23:26:39 +0200

    refactor: remove unused imports from unit test classes

--- src/test/java/com/example/restapi/performance/RendimientoApiTest.java
+++ src/test/java/com/example/restapi/performance/RendimientoApiTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
 
 import com.example.restapi.service.DeustoStreamService;
 import com.github.noconnor.junitperf.JUnitPerfInterceptor;
--- src/test/java/com/example/restapi/unit/config/inicializadorDatosUnitTest.java
+++ src/test/java/com/example/restapi/unit/config/inicializadorDatosUnitTest.java
@@ -1,27 +1,12 @@
 package com.example.restapi.unit.config;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
-import java.util.Collections;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import com.example.restapi.config.inicializarDatos;
-import com.example.restapi.model.Generos;
-import com.example.restapi.model.Pelicula;
-import com.example.restapi.model.Perfil;
-import com.example.restapi.model.Series;
-import com.example.restapi.model.Usuario;
 import com.example.restapi.repository.PeliculaRepository;
 import com.example.restapi.repository.PerfilRepository;
 import com.example.restapi.repository.SerieRepository;
