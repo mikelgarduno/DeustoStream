d6780ae8b19cc1488847d4371b931c8a2a3d746f
ikeralvis <ikeralvis@users.noreply.github.com>
2025-05-10 02:59:03 +0000

    Deploying to gh-pages from @ mikelgarduno/DeustoStream@3027f48346b8c5994fd9a3b3a7bffc747c5089e8 🚀

---
+++ doxygen/classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.js
@@ -0,0 +1,23 @@
+var classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test =
+[
+    [ "setNumeroCapitulos", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#a59cc6ed4a60974494597b0ef345618b0", null ],
+    [ "testEqualsWithDifferentAnio", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#a7f3bb560248e63a034e692145cf8613a", null ],
+    [ "testEqualsWithDifferentClass", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#a35b742bc774b31190af0087fb732d83b", null ],
+    [ "testEqualsWithDifferentDescripcion", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#ac5af6fa6887d223aa69fd1ba3e5b63b0", null ],
+    [ "testEqualsWithDifferentGenero", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#ac2402f3a26a3aaa69598567e0852d9d6", null ],
+    [ "testEqualsWithDifferentId", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#ad43ac134a15af67407f77bbbbc0fc756", null ],
+    [ "testEqualsWithDifferentTitulo", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#ac296aa5f3be7a3d10dfe5d6816091a39", null ],
+    [ "testEqualsWithIdenticalSeries", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#a525742109762d187c2c332da9036f768", null ],
+    [ "testEqualsWithNull", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#acbffae42fd7b35226763a3e8d8e599b6", null ],
+    [ "testEqualsWithSameObject", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#a9cbc0ee0d7c3e02f0649bd46c69a4a69", null ],
+    [ "testGetNumeroCapitulos", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#a4eb78168e3abab3070bceaad663d6ad1", null ],
+    [ "testGettersAndSetters", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#ab9abe9afb2d854294c2ee6ce47fdfeeb", null ],
+    [ "testHashCodeConsistency", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#a9b84f38e0f3e29e2fe569f09bab6cff2", null ],
+    [ "testHashCodeEquality", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#ac89a8fc82e69c6ad46a3c6c8a988aa81", null ],
+    [ "testHashCodeWithDifferentValues", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#ae882bfe491287aa6de5cb0d65d4a8c50", null ],
+    [ "testHashCodeWithNullValues", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#ae0bae59641003a2d0270b76e67499b97", null ],
+    [ "testSerieConstructor", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#ac4ed0a39b1062095e66a45308ca45224", null ],
+    [ "testSerieToString", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#a2f0c6de5f4881700602184bfe0fcc69d", null ],
+    [ "testSetCapitulos", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#a7c297c8804ac83dfbb74ed149ebaedc6", null ],
+    [ "testSetCapitulosWithEmptyList", "classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_serie_test.html#a057985761f7264e8dd651ce3344865b3", null ]
+];
