264d17473324c6668132240c553124ce2d894bbd
ikeralvis <ikeralvis@users.noreply.github.com>
2025-05-14 03:12:33 +0000

    Deploying to gh-pages from @ mikelgarduno/DeustoStream@3027f48346b8c5994fd9a3b3a7bffc747c5089e8 🚀

--- doxygen/classcom_1_1example_1_1restapi_1_1config_1_1inicializar_datos.js
+++
@@ -1,4 +0,0 @@
-var classcom_1_1example_1_1restapi_1_1config_1_1inicializar_datos =
-[
-    [ "run", "classcom_1_1example_1_1restapi_1_1config_1_1inicializar_datos.html#a110c943d44fb0730b383c42ccfd99250", null ]
-];
--- doxygen/search/classes_6.js
+++ doxygen/search/classes_6.js
@@ -2,8 +2,6 @@ var searchData=
 [
   ['pelicula_0',['Pelicula',['../classcom_1_1example_1_1restapi_1_1model_1_1_pelicula.html',1,'com::example::restapi::model']]],
   ['pelicularepository_1',['PeliculaRepository',['../interfacecom_1_1example_1_1restapi_1_1repository_1_1_pelicula_repository.html',1,'com::example::restapi::repository']]],
-  ['peliculatest_2',['PeliculaTest',['../classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_pelicula_test.html',1,'com::example::restapi::unit::modelos']]],
-  ['perfil_3',['Perfil',['../classcom_1_1example_1_1restapi_1_1model_1_1_perfil.html',1,'com::example::restapi::model']]],
-  ['perfilrepository_4',['PerfilRepository',['../interfacecom_1_1example_1_1restapi_1_1repository_1_1_perfil_repository.html',1,'com::example::restapi::repository']]],
-  ['perfiltest_5',['PerfilTest',['../classcom_1_1example_1_1restapi_1_1unit_1_1modelos_1_1_perfil_test.html',1,'com::example::restapi::unit::modelos']]]
+  ['perfil_2',['Perfil',['../classcom_1_1example_1_1restapi_1_1model_1_1_perfil.html',1,'com::example::restapi::model']]],
+  ['perfilrepository_3',['PerfilRepository',['../interfacecom_1_1example_1_1restapi_1_1repository_1_1_perfil_repository.html',1,'com::example::restapi::repository']]]
 ];
